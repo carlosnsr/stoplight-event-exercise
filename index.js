083f0fdf08ab4d807884d1fa2c8ac7cbff99fbf5
Carlos Rodrigues <carlos.all.mail@gmail.com>
2016-12-04 23:38:17 -0700

    Now logs whenever one's mouse enters or leaves a button

--- index.js
+++ index.js
@@ -6,16 +6,34 @@
     var stop_light = document.querySelector('#stopLight')
     stop_light.classList.toggle('stop')
   })
+  stop_button.addEventListener('mouseenter', function (event) {
+    console.log(`Entered ${this.textContent} button`)
+  })
+  stop_button.addEventListener('mouseleave', function (event) {
+    console.log(`Left ${this.textContent} button`)
+  })
 
   var slow_button = document.querySelector('#slowButton')
   slow_button.addEventListener('click', function (event) {
     var slow_light = document.querySelector('#slowLight')
     slow_light.classList.toggle('slow')
   })
+  slow_button.addEventListener('mouseenter', function (event) {
+    console.log(`Entered ${this.textContent} button`)
+  })
+  slow_button.addEventListener('mouseleave', function (event) {
+    console.log(`Left ${this.textContent} button`)
+  })
 
   var go_button = document.querySelector('#goButton')
   go_button.addEventListener('click', function (event) {
     var go_light = document.querySelector('#goLight')
     go_light.classList.toggle('go')
+  })
+  go_button.addEventListener('mouseenter', function (event) {
+    console.log(`Entered ${this.textContent} button`)
+  })
+  go_button.addEventListener('mouseleave', function (event) {
+    console.log(`Left ${this.textContent} button`)
   })
 })();
