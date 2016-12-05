dc78afee3b3ff1e4c4549b488c2e5f1aa3904870
Carlos Rodrigues <carlos.all.mail@gmail.com>
2016-12-04 23:26:00 -0700

    Slow and Go buttons now toggle their respective light's colour

--- index.js
+++ index.js
@@ -6,4 +6,16 @@
     var stop_light = document.getElementById('stopLight')
     stop_light.classList.toggle('stop')
   })
+
+  var slow_button = document.getElementById('slowButton')
+  slow_button.addEventListener('click', function (event) {
+    var slow_light = document.getElementById('slowLight')
+    slow_light.classList.toggle('slow')
+  })
+
+  var go_button = document.getElementById('goButton')
+  go_button.addEventListener('click', function (event) {
+    var go_light = document.getElementById('goLight')
+    go_light.classList.toggle('go')
+  })
 })();
