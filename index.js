f49580c6c47cf92ab32dde7896ee581233c229a6
Carlos Rodrigues <carlos.all.mail@gmail.com>
2016-12-05 00:50:14 -0700

    Moved functions into the parent scope to minimise the number of functions created.
    This is a premature optimisation and has not been verified that it is
    necessary

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 (function() {
   'use strict';
 
+  function log_was_entered(event) {
+    console.log(`Entered ${this.textContent} button`)
+  }
+
+  function log_was_left(event) {
+    console.log(`Left ${this.textContent} button`)
+  }
+
   var buttons = ['stop', 'slow', 'go']
   buttons.forEach(function(type) {
-    var button = document.querySelector(`#${type}Button`)
-    button.addEventListener('click', function change_light(event) {
+    // NOTE: change_light depends on the local variable `type` and so cannot be
+    // lifted out of the forEach() unless we use a closure
+    function change_light(event) {
       var light = document.querySelector(`#${type}Light`)
       light.classList.toggle(type)
-    })
+    }
 
-    button.addEventListener('mouseenter', function log_was_entered(event) {
-      console.log(`Entered ${this.textContent} button`)
-    })
-
-    button.addEventListener('mouseleave', function log_was_left(event) {
-      console.log(`Left ${this.textContent} button`)
-    })
+    var button = document.querySelector(`#${type}Button`)
+    button.addEventListener('click', change_light)
+    button.addEventListener('mouseenter', log_was_entered)
+    button.addEventListener('mouseleave', log_was_left)
   })
+
+  function is_button(element) {
+    return (element && element.classList.contains('button'))
+  }
+
+  function is_light_on(light_type) {
+    return document.querySelector(`#${light_type}Light`)
+      .classList.contains(light_type)
+  }
 
   var controls = document.querySelector('#controls')
   controls.addEventListener('click', function (event) {
-    function is_button(element) {
-      return (element && element.classList.contains('button'))
-    }
-
-    function is_light_on(light_type) {
-      return document.querySelector(`#${light_type}Light`)
-        .classList.contains(light_type)
-    }
-
     var button = event.target
     if (is_button(button)) {
       var text = button.textContent
