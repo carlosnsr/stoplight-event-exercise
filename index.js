091565a0bfbeac9d34d6234bd55e8834c4948fe1
Carlos Rodrigues <carlos.all.mail@gmail.com>
2016-12-05 01:24:44 -0700

    Moved all functions to be declared after their call site
    ...and in order of appearance

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
 (function() {
   'use strict';
 
+  var buttons = ['stop', 'slow', 'go']
+  buttons.forEach(function(type) {
+    var button = document.querySelector(`#${type}Button`)
+    button.addEventListener('click', change_light)
+    button.addEventListener('mouseenter', log_was_entered)
+    button.addEventListener('mouseleave', log_was_left)
+
+    // NOTE: change_light depends on the local variable `type` and so cannot be
+    // lifted out of the forEach() unless we use a closure
+    function change_light(event) {
+      get_light(type).classList.toggle(type)
+    }
+  })
+
+  function get_light(type) {
+    return document.querySelector(`#${type}Light`)
+  }
+
   function log_was_entered(event) {
     console.log(`Entered ${this.textContent} button`)
   }
 
   function log_was_left(event) {
     console.log(`Left ${this.textContent} button`)
-  }
-
-  function get_light(type) {
-    return document.querySelector(`#${type}Light`)
-  }
-
-  var buttons = ['stop', 'slow', 'go']
-  buttons.forEach(function(type) {
-    // NOTE: change_light depends on the local variable `type` and so cannot be
-    // lifted out of the forEach() unless we use a closure
-    function change_light(event) {
-      get_light(type).classList.toggle(type)
-    }
-
-    var button = document.querySelector(`#${type}Button`)
-    button.addEventListener('click', change_light)
-    button.addEventListener('mouseenter', log_was_entered)
-    button.addEventListener('mouseleave', log_was_left)
-  })
-
-  function is_button(element) {
-    return (element && element.classList.contains('button'))
-  }
-
-  function is_light_on(type) {
-    return get_light(type).classList.contains(type)
   }
 
   var controls = document.querySelector('#controls')
@@ -43,4 +35,12 @@
       console.log(`${text} bulb ${is_light_on(text.toLowerCase()) ? 'on' : 'off'}`)
     }
   })
+
+  function is_button(element) {
+    return (element && element.classList.contains('button'))
+  }
+
+  function is_light_on(type) {
+    return get_light(type).classList.contains(type)
+  }
 })();
