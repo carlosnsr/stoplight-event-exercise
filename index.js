8d81ee1c7612da70006ee0a6758db9d51d5929df
Carlos Rodrigues <carlos.all.mail@gmail.com>
2016-12-05 00:23:05 -0700

    Refactored to remove duplicate code into a generic loop

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
 (function() {
   'use strict';
 
-  var stop_button = document.querySelector('#stopButton')
-  stop_button.addEventListener('click', function (event) {
-    var stop_light = document.querySelector('#stopLight')
-    stop_light.classList.toggle('stop')
-  })
-  stop_button.addEventListener('mouseenter', function (event) {
-    console.log(`Entered ${this.textContent} button`)
-  })
-  stop_button.addEventListener('mouseleave', function (event) {
-    console.log(`Left ${this.textContent} button`)
-  })
+  var buttons = ['stop', 'slow', 'go']
+  buttons.forEach(function(type) {
+    var button = document.querySelector(`#${type}Button`)
+    button.addEventListener('click', function change_light(event) {
+      var light = document.querySelector(`#${type}Light`)
+      light.classList.toggle(type)
+    })
 
-  var slow_button = document.querySelector('#slowButton')
-  slow_button.addEventListener('click', function (event) {
-    var slow_light = document.querySelector('#slowLight')
-    slow_light.classList.toggle('slow')
-  })
-  slow_button.addEventListener('mouseenter', function (event) {
-    console.log(`Entered ${this.textContent} button`)
-  })
-  slow_button.addEventListener('mouseleave', function (event) {
-    console.log(`Left ${this.textContent} button`)
-  })
+    button.addEventListener('mouseenter', function log_was_entered(event) {
+      console.log(`Entered ${this.textContent} button`)
+    })
 
-  var go_button = document.querySelector('#goButton')
-  go_button.addEventListener('click', function (event) {
-    var go_light = document.querySelector('#goLight')
-    go_light.classList.toggle('go')
-  })
-  go_button.addEventListener('mouseenter', function (event) {
-    console.log(`Entered ${this.textContent} button`)
-  })
-  go_button.addEventListener('mouseleave', function (event) {
-    console.log(`Left ${this.textContent} button`)
+    button.addEventListener('mouseleave', function log_was_left(event) {
+      console.log(`Left ${this.textContent} button`)
+    })
   })
 
   var controls = document.querySelector('#controls')
